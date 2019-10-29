991ad0f9482498cccbd3a2358b9c28821c3a47d3
jlb1999 <jlbroughton88@gmail.com>
2019-10-29 07:25:23 -0400

    added procfile

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000 , () => {
     console.log("Server listening at port 3000");
 })
