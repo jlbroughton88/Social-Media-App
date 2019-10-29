0c6e578543a4027efe00d746f2c623ac8ba986aa
jlb1999 <jlbroughton88@gmail.com>
2019-10-29 08:10:32 -0400

    react added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path")
 require("dotenv").config();
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -12,10 +13,22 @@ mongoose.connect(MONGODB_URI, {
     console.log("Mongodb Connected!")
 })
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"))
+    })
+  } else {
+    app.use(express.static(path.join(__dirname, "/client/public")));
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/public/index.html"))
+    })
+  }
+
 app.get("/", (req, res) => {
     res.send("Hello")
 });
 
-app.listen(process.env.PORT || 3000 , () => {
+app.listen(process.env.PORT || 5000 , () => {
     console.log("Server listening at port 3000");
 });
